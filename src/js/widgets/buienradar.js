b002918a9ce59a183f24b1bb116a7ac4c8340d78
Bas van Dijk <bas@usto.nl>
2016-05-20 08:45:00 +0200

    Fixed bug with image refreshing, increased update interval to 5 minutes

--- src/js/widgets/buienradar.js
+++ src/js/widgets/buienradar.js
@@ -1,13 +1,15 @@
 function updateBuienRadar(gridCell, widgetConfig) {
 
-    gridCell.css('background-image', 'url(http://buienradar.nl/image?type=zozw)');
+    var uniqueString = (new Date()).getTime();
+
+    gridCell.css('background-image', 'url(http://buienradar.nl/image?type=zozw&' + uniqueString + ')');
     gridCell.css('background-size', 'cover');
     gridCell.css('background-position', 'center center');
     gridCell.css('background-repeat', 'no-repeat');
 
     setTimeout(function() {
         updateBuienRadar(gridCell, widgetConfig);
-    }, 60000);
+    }, 300000);
 }
 
 widgets.buienradar = updateBuienRadar;
